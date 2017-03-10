4d04ff5eb5e42845037ec94393fd09d09dd9f713
Julien BONACHERA <jbonachera@xebia.fr>
2017-03-10 18:44:23 +0100

    version bump

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ void loopHandler() {
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
-  Homie_setFirmware("intercom", "1.0.5");
+  Homie_setFirmware("intercom", "1.0.7");
   Homie_setBrand("vx-labs")
   intercomNode.advertise("unit");
   intercomNode.advertise("room");
