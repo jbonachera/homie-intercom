e3d9343cd38d8e09f8fe3d06ea17cae72514c9d7
Julien BONACHERA <jbonachera@xebia.fr>
2017-03-11 19:02:15 +0100

    implement new observed behaviour

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
+snapshot: commit-version build
+
+release: tag-version build
+
+commit-version:
+	echo "#define VERSION \"$$(git describe --tags)-$$(git rev-parse --short HEAD)\"" > src/version.h
+
+tag-version:
+	echo "#define VERSION \"$$(git describe --tags)\"" > src/version.h
+
 build:
 	docker run -v $$(pwd)/.pio_home:/root/.platformio -v $$(pwd):/usr/local/src --rm $$(docker build -q -f Dockerfile.build .)
 	md5sum release/homie-intercom > release/homie-intercom.md5
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 #include <Homie.h>
+#include <version.h>
 
-#define PINSENSOR 13
-#define PINOUT 12
+#define PINSENSOR 13 //d7
 #define SENSOR_IS_RINGING LOW
-const int INTERCOM_INTERVAL = 100;
+#define INTERCOM_INTERVAL 100
 bool state = false;
-bool future_state = false;
 unsigned long lastPoll = 0;
-unsigned long update_date = 0;
 
 
 HomieSetting<const char*> roomSetting("room", "The room this sensor will monitor");  // id, description
@@ -17,31 +15,22 @@ HomieNode intercomNode("intercom", "intercom");
 
 void setupHandler() {
   pinMode(PINSENSOR, INPUT_PULLUP);
-  pinMode(PINOUT, OUTPUT);
-  digitalWrite(PINOUT, LOW);
   intercomNode.setProperty("unit").send("bool");
   intercomNode.setProperty("room").send(roomSetting.get());
   intercomNode.setProperty("ringing").setRetained(true).send("false");
 }
 
 void intercomLoopHander(){
-  bool new_state = state;
   if (millis() - lastPoll >= INTERCOM_INTERVAL * 1UL) {
-    new_state = digitalRead(PINSENSOR) == SENSOR_IS_RINGING;
-    if (future_state != state) {
-      if (millis() - update_date > 150){
-        if (future_state) {
-          intercomNode.setProperty("ringing").setRetained(true).send("true");
-          digitalWrite(PINOUT, HIGH);
-        } else {
-          intercomNode.setProperty("ringing").setRetained(true).send("false");
-          digitalWrite(PINOUT, LOW);
-        }
-        state = future_state;
+    bool new_state = digitalRead(PINSENSOR) == SENSOR_IS_RINGING;
+    if (new_state != state) {
+      if (new_state) {
+        intercomNode.setProperty("ringing").setRetained(true).send("true");
+        
+      } else {
+        intercomNode.setProperty("ringing").setRetained(true).send("false");
       }
-    } else if (new_state != state) {
-      future_state = new_state;
-      update_date = millis();
+      state = new_state;
     }
     lastPoll = millis();
   }
@@ -54,11 +43,12 @@ void loopHandler() {
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
-  Homie_setFirmware("intercom", "1.0.9");
+  Homie_setFirmware("intercom", VERSION);
   Homie_setBrand("vx-labs")
   intercomNode.advertise("unit");
   intercomNode.advertise("room");
   intercomNode.advertise("ringing");
+  intercomNode.advertise("tension");
   Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
 
   roomSetting.setDefaultValue("NoRoom").setValidator([] (const char* candidate) {
